274771a33705a7e451c2328db1bde865be38f36e
kkjjkkll1111 <kkjjkkll1111@gmail.com>
2025-03-18 15:48:45 +0900

    Update code.py

--- week3/code.py
+++ week3/code.py
@@ -1,1 +1,43 @@
+import serial
+from influxdb_client import InfluxDBClient
+import time
 
+serial_port = "COM8"
+baud_rate = 9600
+timeout = 2
+
+influxdb_url = "http://localhost:8086"
+influxdb_token = "AxwQ6GEbD1pjTiphpAD7x2MU2shuBOOsCEhwD6STSwgMRas-4ktfyGyDGsloXvylk1oWhe8dRKPXxhPARzUyZw=="
+influxdb_org = "test"
+influxdb_bucket = "dust"
+
+client = InfluxDBClient(url=influxdb_url, token = influxdb_token, org = influxdb_org)
+write_api = client.write_api()
+
+try:
+    ser = serial.Serial(serial_port, baud_rate, timeout = timeout)
+    print(f"Connected to {serial_port} at {baud_rate} baud")
+except:
+    print("Failed to connect to serial port")
+
+try:
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').strip()
+
+            if "=" in line:
+                key, value = line.split("=")
+                try:
+                    value = float(value)
+                    data=f"sensor_data,device=arduino {key}={value}"
+                    write_api.write(bucket=influxdb_bucket, record=data)
+                    print(f"Data written to influxDB: {key}={value}")
+                except:
+                    print("Invalid data format")
+            time.sleep(1)
+
+except KeyboardInterrupt:
+            print("프로그램이 종료되었습니다.")
+
+finally:
+    ser.close()
